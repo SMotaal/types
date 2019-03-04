2196a3dc7733527fe61daf26bc41940f79ed0edb
Saleh <saleh@grasppe.com>
2019-03-04 06:05:29 -0500

    Update markout preview

--- markout/preview.js
+++ markout/preview.js
@@ -2,12 +2,11 @@
 
 typeof document === 'object' &&
 	typeof location === 'object' &&
-	(async () => {
+	(() => {
 		let title, href, hash, type;
 
-		const section = document.querySelector('markout-content:not([src])');
-
-		if (!section) return;
+		const section =
+			document.querySelector('markout-content') || document.body.appendChild(document.create('markout-content'));
 
 		const link = document.head.querySelector(
 			'link[rel="alternate" i][type^="text/markout" i][type^="text/markdown" i][type^="text/md" i][href], link[rel="alternate" i][href$=".md" i][href$=".markdown" i], link[rel="alternate" i][href]',
@@ -21,30 +20,20 @@ typeof document === 'object' &&
 			  }`
 			: `${new URL('./', location)}`;
 
-		const src =
-			(link && ({href, title} = link) && href) ||
-			((hash = location.hash) && (hash = hash.trim().slice(1)) && (href = `${new URL(hash, location.origin)}`)) ||
-			((title = 'Markout'), `${base}./markout/README.md`);
+		if (!section.hasAttribute('src')) {
+			const src =
+				(link && ({href, title} = link) && href) ||
+				((hash = location.hash) && (hash = hash.trim().slice(1)) && (href = `${new URL(hash, location.origin)}`)) ||
+				((title = 'Markout'), `${base}./markout/README.md`);
 
-		title ||
-			((title = `${href.replace(/(.*?)((?:[^/]+?[/]?){1,2})(?:\..*|)$/, '$2')}`.trim()) &&
-				(document.title = `${title} — Markout`));
+			title ||
+				((title = `${href.replace(/(.*?)((?:[^/]+?[/]?){1,2})(?:\..*|)$/, '$2')}`.trim()) &&
+					(document.title = `${title} — Markout`));
+
+			section.setAttribute('src', src);
+		}
 
 		addEventListener('hashchange', () => location.reload());
-
-		// section.rewriteAnchors = anchors => {
-		// 	for (const anchor of anchors) {
-		// 		!anchor || anchor.hash || !anchor.href || (anchor.href = `#${anchor.href}`);
-		// 	}
-		// };
-		// addEventListener('hashchange', () => {
-		// 	const src =
-		// 		(hash = location.hash) && (hash = hash.trim().slice(1)) && (href = `${new URL(hash, location.origin)}`);
-
-		// 	src && section.load(src);
-		// });
-
-		section.setAttribute('src', src);
 
 		const lib = `${base}quench/${
 			/^\?dev\b|\&dev\b/i.test(location.search) ? 'lib/browser/markout.js' : 'dist/markout.m.js'
@@ -52,3 +41,15 @@ typeof document === 'object' &&
 
 		dynamicImport(lib);
 	})();
+
+// section.rewriteAnchors = anchors => {
+// 	for (const anchor of anchors) {
+// 		!anchor || anchor.hash || !anchor.href || (anchor.href = `#${anchor.href}`);
+// 	}
+// };
+// addEventListener('hashchange', () => {
+// 	const src =
+// 		(hash = location.hash) && (hash = hash.trim().slice(1)) && (href = `${new URL(hash, location.origin)}`);
+
+// 	src && section.load(src);
+// });
