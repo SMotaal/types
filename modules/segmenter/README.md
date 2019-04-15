175ef9e72ef3252b9d263c45591534fef2a80217
Saleh <saleh@grasppe.com>
2019-04-14 20:30:14 -0400

    Add `modules/segmenter`

---
+++ modules/segmenter/README.md
@@ -0,0 +1,1 @@
+﻿# `smotaal.io/modules/segmenter`
---
+++ modules/segmenter/segmenter.js
@@ -0,0 +1,90 @@
+﻿export class Segmenter extends RegExp {
+	/**
+	 * @param {string | RegExp} pattern
+	 * @param {string} [flags]
+	 * @param {(string|undefined)[]} [types]
+	 */
+	constructor(pattern, flags, types) {
+		(pattern &&
+			pattern.types &&
+			Symbol.iterator in pattern.types &&
+			((!types && (types = pattern.types)) || types === pattern.types)) ||
+			Object.freeze((types = (types && Symbol.iterator in types && [...types]) || []));
+		const {LOOKAHEAD = Segmenter.LOOKAHEAD, INSET = Segmenter.INSET, UNKNOWN = Segmenter.UNKNOWN} = new.target;
+		Object.defineProperties(super(pattern, flags), {
+			types: {value: types, enumerable: true},
+			LOOKAHEAD: {value: LOOKAHEAD},
+			INSET: {value: INSET},
+			UNKNOWN: {value: UNKNOWN},
+			// lookaheads: {value: (typeof LOOKAHEAD === 'symbol' && types.indexOf(LOOKAHEAD) + 1) || false},
+			// insets: {value: (typeof insets === 'symbol' && types.indexOf(INSET) + 1) || false},
+		});
+	}
+
+	/**
+	 * @param {RegExpExecArray} match
+	 */
+	matchType(text, index) {
+		return index > 0 && text !== undefined && match.types[index - 1] != null;
+	}
+
+	capture(text, index, match) {
+		// let typeOf;
+		if (index === 0 || text === undefined) return;
+
+		const typeIndex = index - 1;
+		const type = this.types[typeIndex];
+
+		if (type === INSET) {
+			match.inset = text;
+			return;
+		} else if (type === LOOKAHEAD) {
+			match.lookahead = text;
+			return;
+		} else if (type !== UNKNOWN) {
+			switch (typeof type) {
+				case 'string':
+					if (match.typeIndex > -1) return;
+					match.type = type;
+					match.typeIndex = typeIndex;
+				case 'symbol':
+					match[type] = text;
+					return;
+				case 'function':
+					type(text, index, match);
+					return;
+			}
+		}
+	}
+
+	/**
+	 * @param {RegExpExecArray} match
+	 * @returns {typeof match & {slot: number, type: string}}
+	 */
+	exec(source) {
+		const match = super.exec(source);
+		match &&
+			((match.typeIndex = -1),
+			match.forEach(this.capture || Segmenter.prototype.capture, this),
+			match.typeIndex > -1 || ((match.type = 'unknown'), (match.typeIndex = -1)),
+			null);
+
+		return match;
+  }
+
+	static define(factory, flags) {
+		const types = [];
+		const RegExp = (this && (this.prototype === Segmenter || this.prototype instanceof Segmenter) && this) || Segmenter;
+    const pattern = factory(type => (types.push((type != null || undefined) && type), ''));
+
+    flags = `${(flags == null ? pattern && pattern.flags : flags) || ''}`;
+
+		return new RegExp(pattern, flags, types);
+	}
+}
+
+export const {INSET, UNKNOWN, LOOKAHEAD} = Object.defineProperties(Segmenter, {
+	INSET: {value: Symbol.for('INSET'), enumerable: true},
+	UNKNOWN: {value: Symbol.for('UNKNOWN'), enumerable: true},
+	LOOKAHEAD: {value: Symbol.for('LOOKAHEAD'), enumerable: true},
+});
