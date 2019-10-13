4391e4f36f7daef2cf6b50d0a6f4b2b2ecaea329
Saleh Abdel Motaal <saleh@grasppe.com>
2019-10-13 18:31:26 -0400

    Update detect-dark-mode.js

--- browser/detect-dark-mode.js
+++ browser/detect-dark-mode.js
@@ -1,61 +1,69 @@
-﻿// true || //
-typeof document !== 'object' ||
-	!document ||
-	!document.body ||
-	document.darkModeDetector ||
-	console.log(
-		(document.darkModeDetector = new (class DarkModeDetector {
-			constructor() {
-				this.labels = {undefined: '⚙︎', light: '', dark: '☾'};
-				// SEE: https://github.com/WebKit/webkit/blob/master/Source/WebCore/css/CSSValueKeywords.in
-				this.color = '-webkit-text';
-				this.span = document.createElement('div');
-				this.span.style.position = 'fixed';
-				this.span.style.bottom = this.span.style.left = 0;
-				this.span.style.opacity = 0.25;
-				this.span.style.color = this.color;
-				this.span.style.padding = '0.25em';
-				// this.span.style.writingMode = 'vertical-rl';
-				// this.span.style.textOrientation = 'upright';
-				this.span.inert = true;
-				this.span.text = this.span.attachShadow({mode: 'closed'}).appendChild(new Text(this.labels[undefined]));
-				document.body.appendChild(this.span);
-				// this.target = this.span;
-				this.target = document.body;
-				this.state = {
-					currentMode: undefined,
-					previousMode: undefined,
-					currentThreshold: undefined,
-					previousThreshold: undefined,
-				};
-				this.update = threshold => {
-					if (
-						(threshold = parseFloat(threshold) || 0) === this.threshold ||
-						this.mode === (this.mode = (this.threshold = threshold) > 127 ? 'light' : 'dark')
-					)
-						return;
-					document.dispatchEvent(
+﻿class DarkModeDetector {
+	/**
+	 * @param {HTMLElement} span
+	 * @param {Record<'undefined'|'light'|'dark', string> labels
+	 */
+	constructor(span, labels) {
+		let internal = {};
+		internal.labels = {light: '☀︎', dark: '☾', undefined: '•'};
+		labels &&
+			(`${labels.light}` && (internal.labels.light = `${labels.light}`),
+			`${labels.dark}` && (internal.labels.dark = `${labels.dark}`),
+			`${labels.undefined}` && (internal.labels.undefined = `${labels.undefined}`));
+		span || (span = globalThis.document.createElement('span'));
+		(internal.root = span.attachShadow({mode: 'closed'})).append(
+			(internal.label = new Text(internal.labels[undefined])), //
+			(internal.target = (internal.document = span.ownerDocument).createElement('span')), //
+		);
+		// SEE: https://github.com/WebKit/webkit/blob/master/Source/WebCore/css/CSSValueKeywords.in
+		internal.target.style.color = '-webkit-text';
+		span.style.color === 'initial' && (span.style.color = internal.target.style.color);
+		internal.target.computedStyleMap && (internal.style = Object.freeze(internal.target.computedStyleMap()));
+		internal.update = this.update.bind(this, internal);
+		internal.timer = setInterval(internal.update, 1000);
+		Object.preventExtensions(
+			Object.defineProperties(this, {
+				update: {value: internal.update, enumerable: false, writable: false, configurable: false},
+				'(internal)': {value: internal, enumerable: false, writable: false, configurable: false},
+				mode: {enumerable: true, writable: true, configurable: false},
+				color: {enumerable: true, writable: true, configurable: false},
+				threshold: {enumerable: true, writable: true, configurable: false},
+			}),
+		);
+		internal = null;
+	}
+
+	update(internal) {
+		internal.target.isConnected || internal.document.body.appendChild(internal.root.host);
+		internal.threshold !==
+			(internal.threshold = this.threshold =
+				255 -
+				parseFloat(
+					(/\b\d{1,3}|$/.exec(
+						(internal.color = this.color = `${
+							internal.style
+								? internal.style.get('color')
+								: internal.document.defaultView.getComputedStyle(internal.target)['color']
+						}`),
+					) || '0')[0],
+				)) &&
+			internal.mode !==
+				(internal.mode = this.mode = (internal.threshold = internal.threshold) > 127 ? 'light' : 'dark') &&
+			(((internal.label.textContent = internal.labels[internal.mode] || internal.mode), internal.state == null)
+				? (internal.state = {...this})
+				: internal.document.dispatchEvent(
 						new CustomEvent('color-scheme-change', {
-							detail: (this.state = {
-								currentMode: this.mode,
-								previousMode: this.state.currentMode,
-								currentThreshold: this.threshold,
-								previousThreshold: this.state.currentThreshold,
-							}),
+							detail: {instance: this, previousState: {...internal.state}, ...(internal.state = {...this})},
 						}),
-					);
-					console.log(this.mode);
-					this.span.text.textContent = this.labels[this.mode];
-				};
-				this.handler = () => {
-					this.target.style.currentColor = this.target.style.color;
-					this.target.style.color = this.color;
-					this.update(255 - (/\b\d{1,3}|$/.exec(window.getComputedStyle(this.target).color) || '0')[0]);
-					this.target.style.color = this.target.style.currentColor;
-				};
-				this.timer = setInterval(this.handler, 1000);
-			}
-			// this.handler();
-			// debugger;
-		})()),
-	);
+				  ));
+	}
+}
+
+globalThis.document.darkModeDetector ||
+	((globalThis.document.darkModeDetector = new DarkModeDetector(
+		Object.assign(globalThis.document.createElement('div'), {
+			style: `position: fixed; bottom: 0; left: 0; opacity: 0.25; padding: 0.25em; color: initial;`,
+			inert: true,
+		}),
+	)),
+	globalThis.document.addEventListener('color-scheme-change', console.log));
