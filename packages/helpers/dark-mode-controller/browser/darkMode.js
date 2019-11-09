16b6727890533a8d8143b8518ac23c53a0d88472
Saleh Abdel Motaal <saleh@smotaal.io>
2019-11-09 12:35:45 -0500

    Create darkMode.js

---
+++ packages/helpers/dark-mode-controller/browser/darkMode.js
@@ -0,0 +1,11 @@
+﻿import {DarkModeController} from '../dark-mode-controller.js';
+
+/** @type {{enable(): void; disable(): void; toggle(state?: boolean | 'auto'): void;}} */
+export const darkMode =
+	(typeof document === 'object' &&
+		document &&
+		//@ts-ignore
+		(document.darkMode ||
+			//@ts-ignore
+			(document.darkMode = new (Object.preventExtensions(DarkModeController))()))) ||
+	undefined;
